ba30a30abf29bdd223cdc08a27132a7683d986d4
marijan <marijan.kozic@lss.hr>
2016-05-18 18:22:26 +0200

    dovrsi prelazak na State Machine

--- PotapanjeBrodova/AITemplate.cs
+++ PotapanjeBrodova/AITemplate.cs
@@ -50,62 +50,21 @@ namespace PotapanjeBrodova
             return this.gadjanoPolje;
         }
 
-        // OVDJE TREBA IZBACITI SVE KEREFEKE I OSTAVITI SAMO NUZNU OBRADU GADJANJA
-        // upisivanje vrijednosti u potrebne varijable i uklanjanje iz mreze
         public void ObradiPogodak(rezultatGadjanja rezultat) {
             this.rezultatGadjanja = rezultat;
-            Polje p = this.gadjanoPolje;
-            // NAPIPAVANJE
-            if (this.rezim == rezimRada.napipavanje) {
-                switch (rezultat) {
-                    case rezultatGadjanja.promasaj:
-                        // obicni promasaj
-                        this.Mreza.EliminirajPolje(p);
-                        Izvazi();
-                        this.gadjanoPolje = SlijedecePoljeNapipavanje();
-                        break;
-                    case rezultatGadjanja.pogodak:
-                        // prvi pogodak
-                        this.rezim = rezimRada.unistavanje;
-                        this.trenutnaMeta.Add(p);
-                        this.Mreza.EliminirajPolje(p);
-                        this.gadjanoPolje = SlijedecePoljeUnistavanje(p, rezultat);
-                        break;
-                    case rezultatGadjanja.potopljen:
-                        // ovdje dolazimo samo ako slucajno napipamo brod velicine 1
-                        this.trenutnaMeta.Add(p);
-                        this.Flota.Remove(this.trenutnaMeta.Count);
-                        EliminirajBrod(trenutnaMeta);
-                        this.trenutnaMeta.Clear();
-                        this.gadjanoPolje = SlijedecePoljeNapipavanje();
-                        break;
-                }
+            switch (rezultat) {
+                case rezultatGadjanja.pogodak:
+                    this.trenutnaMeta.Add(this.gadjanoPolje);
+                    break;
+                case rezultatGadjanja.potopljen:
+                    this.trenutnaMeta.Add(this.gadjanoPolje);
+                    this.flota.Remove(this.trenutnaMeta.Count);
+                    this.trenutnaMeta.Clear();
+                    break;
+                default:
+                    break;
             }
-            // UNISTAVANJE
-            else {
-                switch (rezultat) {
-                    case rezultatGadjanja.promasaj:
-                        this.Mreza.EliminirajPolje(p);
-                        this.gadjanoPolje = SlijedecePoljeUnistavanje(p, rezultat);
-                        break;
-                    case rezultatGadjanja.pogodak:
-                        this.Mreza.EliminirajPolje(p);
-                        this.trenutnaMeta.Add(p);
-                        this.gadjanoPolje = SlijedecePoljeUnistavanje(p, rezultat);
-                        break;
-                    case rezultatGadjanja.potopljen:
-                        // ako smo potopili brod, cistimo varijable i vracamo se na napipavanje
-                        this.moguciSmjerovi.Clear();
-                        this.pronadjeniSmjer = smjer.nepoznato;
-                        this.trenutnaMeta.Add(p);
-                        this.Flota.Remove(this.trenutnaMeta.Count);
-                        EliminirajBrod(this.trenutnaMeta);
-                        this.trenutnaMeta.Clear();
-                        this.rezim = rezimRada.napipavanje;
-                        this.gadjanoPolje = SlijedecePoljeNapipavanje();
-                        break;
-                }
-            }
+            this.Mreza.EliminirajPolje(this.gadjanoPolje);
         }
 
         public abstract void EliminirajBrod(List<Polje> brod);
@@ -140,91 +99,6 @@ namespace PotapanjeBrodova
                     }
                 }
             }
-        }
-
-        virtual public Polje SlijedecePoljeNapipavanje() {
-            var najtezi = this.Mreza.polja.Max(x => x.Tezina);
-            var najtezaGrupa = this.Mreza.polja.FindAll(x => x.Tezina == najtezi);
-            return najtezaGrupa.ElementAt(rand.Next(najtezaGrupa.Count));
-        }
-
-        protected Polje SlijedecePoljeUnistavanje(Polje prethodnoPolje, rezultatGadjanja rezultat) {
-            Polje zadnjiPogodak = this.trenutnaMeta.Last();
-            Polje prviPogodak = this.trenutnaMeta.First();
-            if (this.moguciSmjerovi.Count == 0) {
-                moguciSmjerovi = IzracunajMoguceSmjerove(prviPogodak);
-            }
-
-            // biramo smjer:
-            // 1) ako je poznat, nastavljamo po istom
-            // 2) ako smo do sada pogodili samo jedno polje, biramo nasumicno neki od preostalih smjerova
-            // 3) ako smo pogodili dva polja, iz njih racunamo smjer
-            // 4) ako smo imali smjer i upravo promasili, biramo suprotni smjer
-            // 5) ako smo promasili a nismo imali smjer, mijenjamo nasumicno smjer
-
-            // Smjer je poznat: slucajevi 1) i 4)
-            if (this.pronadjeniSmjer != smjer.nepoznato) {
-
-                // 1)
-                if (rezultat == rezultatGadjanja.pogodak) {
-                    switch (this.pronadjeniSmjer) {
-                        case smjer.gore:
-                            return new Polje(zadnjiPogodak.Redak - 1, zadnjiPogodak.Stupac);
-                        case smjer.dolje:
-                            return new Polje(zadnjiPogodak.Redak + 1, zadnjiPogodak.Stupac);
-                        case smjer.lijevo:
-                            return new Polje(zadnjiPogodak.Redak, zadnjiPogodak.Stupac - 1);
-                        case smjer.desno:
-                            return new Polje(zadnjiPogodak.Redak, zadnjiPogodak.Stupac + 1);
-                    }
-                }
-                else {
-
-                    // 4)
-                    switch (this.pronadjeniSmjer) {
-                        case smjer.gore:
-                            this.pronadjeniSmjer = smjer.dolje;
-                            return new Polje(zadnjiPogodak.Redak + 1, zadnjiPogodak.Stupac);
-                        case smjer.dolje:
-                            this.pronadjeniSmjer = smjer.gore;
-                            return new Polje(zadnjiPogodak.Redak - 1, zadnjiPogodak.Stupac);
-                        case smjer.lijevo:
-                            this.pronadjeniSmjer = smjer.desno;
-                            return new Polje(zadnjiPogodak.Redak, zadnjiPogodak.Stupac + 1);
-                        case smjer.desno:
-                            this.pronadjeniSmjer = smjer.lijevo;
-                            return new Polje(zadnjiPogodak.Redak, zadnjiPogodak.Stupac - 1);
-                    }
-                }
-            }
-            else {
-                // Smjer je nepoznat: slucajevi 2), 3) i 5)
-
-                // 2) prvi pogodak ili 5) promasaj smjera
-                if (this.trenutnaMeta.Count == 1) {
-                    smjer noviSmjer = moguciSmjerovi.ElementAt(rand.Next(moguciSmjerovi.Count));
-                    moguciSmjerovi.Remove(noviSmjer);
-                    switch (noviSmjer) {
-                        case smjer.gore:
-                            return new Polje(zadnjiPogodak.Redak - 1, zadnjiPogodak.Stupac);
-                        case smjer.dolje:
-                            return new Polje(zadnjiPogodak.Redak + 1, zadnjiPogodak.Stupac);
-                        case smjer.lijevo:
-                            return new Polje(zadnjiPogodak.Redak, zadnjiPogodak.Stupac - 1);
-                        case smjer.desno:
-                            return new Polje(zadnjiPogodak.Redak, zadnjiPogodak.Stupac + 1);
-                    }
-                }
-                else {
-                    // 3) upravo smo pogodili drugo polje (za 3 i ostala polja vec imamo odredjen smjer)
-                    this.pronadjeniSmjer = OdrediSmjer(this.trenutnaMeta.First(), prethodnoPolje);
-                    // pozovimo rekurzivno sami sebe da izracunamo novo polje, sada kada znamo smjer
-                    return SlijedecePoljeUnistavanje(prethodnoPolje, rezultat);
-                }
-            }
-
-            // ova glupost je samo zato sto VS misli da imamo rupu u vracanju vrijednosti
-            return new Polje(-1, -1);
         }
 
         protected smjer OdrediSmjer(Polje prvo, Polje drugo) {
